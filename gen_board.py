02fb939bd53c29c64f93d579e737d29980d760f2
Nicole Pellizzon <nipel@dtu.dk>
2023-03-14 11:16:28 +0100

    state class almost implemented

--- gen_board.py
+++ gen_board.py
@@ -2,15 +2,20 @@ import numpy as np
 import time
 import sys
 
-class Peg:
+class State:
+    def __init__(self, board_size):
+        self.children: dict = {}
+        self.board = np.full((board_size, board_size),3)
+        self.parent: State = None
 
+class Peg:
     def __init__(self):
-        #change to userinput after development
+        # change to user input after development
         self.board_size = 5
         self.completed_moves = []
         self.frontier = []
         self.won = False
-        self.board = np.full((self.board_size,self.board_size),3)
+        self.state_dictionary = {}
 
         ## 0 1 2 3 4 
         #0 x x x x x
@@ -20,13 +25,17 @@ class Peg:
         #4 x
         #
 
-        # print(self.board[0][:])
+        # instantiate initial state and update nodes
+        initial_state = State(self.board_size)
+        self.state_dictionary[0] = initial_state
+
         for i in range(self.board_size):
-            # print(self.self.board_size-i)
-            self.board[i,0:self.board_size-i] = 1
+            initial_state.board[i,0:self.board_size-i] = 1
 
         # setting random empty position, will be hardcoded for development
-        self.board[0,0] = 0
+        self.state_dictionary[0].board[0,0] = 0
+
+        self.print_board(initial_state)
 
     # Checking if the move is out of bounds (OoB)
     def check_OoB(self, i, j):
@@ -35,18 +44,19 @@ class Peg:
         else:
             return True
     
-    def board_return(self):
-        return self.board
+    def board_return(self, state):
+        return state.board
     
     # Illustrating the self.board in a nice way
-    def print_board(self):
+    def print_board(self, state):
         for i in range(self.board_size):
-            print(self.board[i,0:self.board_size-i])
+            print(state.board[i, 0:self.board_size-i])
 
-    def num_pegs(self):
-        return np.count_nonzero(self.board == 1)
 
-    def check_move(self):
+    def num_pegs(self, state):
+        return np.count_nonzero(state.board == 1)
+
+    def check_move(self, state):
         # valid_moves = np.array([0,0,0,0,0,0])
         self.valid_moves = []
         # checking if each peg can move
@@ -54,92 +64,97 @@ class Peg:
             for j in range(self.board_size):
                 # checking move
                 # S we want (2,0) (i,j) to print((4,0))
-                if self.board[i,j] == 1 and i+2 < self.board_size and self.board[i+2,j] == 0 and self.board[i+1,j] == 1 and self.check_OoB(i, j):
-                    # print("S",i,j, " -> ", i+2, j)
-                    # valid_moves = np.vstack([valid_moves, [i,j,i+1,j,i+2,j]])
+                if state.board[i,j] == 1 and i+2 < self.board_size and state.board[i+2,j] == 0 and state.board[i+1,j] == 1 and self.check_OoB(i, j):
                     self.valid_moves.append([i,j,i+1,j,i+2,j])
                 # N 
-                if self.board[i,j] == 1 and i-2 >= 0 and self.board[i-2,j] == 0 and self.board[i-1,j] == 1 and self.check_OoB(i, j):
-                    # print("N",i,j, " -> ", i-2, j)   
-                    # valid_moves = np.vstack([valid_moves, [i,j,i-1,j,i-2,j]])      
+                if state.board[i,j] == 1 and i-2 >= 0 and state.board[i-2,j] == 0 and state.board[i-1,j] == 1 and self.check_OoB(i, j):
                     self.valid_moves.append([i,j,i-1,j,i-2,j])
                 # E
-                if self.board[i,j] == 1 and j+2 < self.board_size and self.board[i,j+2] == 0 and self.board[i,j+1] == 1 and self.check_OoB(i, j):
-                    # print("E",i,j, " -> ", i, j+2)
-                    # valid_moves = np.vstack([valid_moves, [i,j,i,j+1,i,j+2]])
+                if state.board[i,j] == 1 and j+2 < self.board_size and state.board[i,j+2] == 0 and state.board[i,j+1] == 1 and self.check_OoB(i, j):
                     self.valid_moves.append([i,j,i,j+1,i,j+2])
                 # W 
-                if self.board[i,j] == 1 and j-2 >= 0 and self.board[i,j-2] == 0 and self.board[i,j-1] == 1 and self.check_OoB(i, j):
-                    # print("W",i,j, " -> ", i, j-2)
-                    # valid_moves = np.vstack([valid_moves, [i,j,i,j-1,i,j-2]])
+                if state.board[i,j] == 1 and j-2 >= 0 and state.board[i,j-2] == 0 and state.board[i,j-1] == 1 and self.check_OoB(i, j):
                     self.valid_moves.append([i,j,i,j-1,i,j-2])
-
                 # SW
-                if self.board[i,j] == 1 and i+2 < self.board_size and j-2 >= 0 and self.board[i+2,j-2] == 0 and self.board[i+1,j-1] == 1 and self.check_OoB(i, j):
-                    # print("SW", i, j, " -> ", i+2, j-2)
-                    # valid_moves = np.vstack([valid_moves, [i,j,i+1,j-1,i+2,j-2]])
+                if state.board[i,j] == 1 and i+2 < self.board_size and j-2 >= 0 and state.board[i+2,j-2] == 0 and state.board[i+1,j-1] == 1 and self.check_OoB(i, j):
                     self.valid_moves.append([i,j,i+1,j-1,i+2,j-2])
-
                 # NE
-                if self.board[i,j] == 1 and i-2 >= 0 and j+2 < self.board_size and self.board[i-2,j+2] == 0 and self.board[i-1,j+1] == 1 and self.check_OoB(i, j):
-                    # print("NE", i, j, " -> ", i-2, j+2)
-                    # valid_moves = np.vstack([valid_moves, [i,j,i-1,j+1,i-2,j+2]])
+                if state.board[i,j] == 1 and i-2 >= 0 and j+2 < self.board_size and state.board[i-2,j+2] == 0 and state.board[i-1,j+1] == 1 and self.check_OoB(i, j):
                     self.valid_moves.append([i,j,i-1,j+1,i-2,j+2])
-        
         return self.valid_moves
                 
-    def move(self, action):
-        print("MOVING :: ",action)
-        self.board[action[0],action[1]] = 0
-        self.board[action[2],action[3]] = 0
-        self.board[action[4],action[5]] = 1
-        self.print_board()
+
+    def move(self, action, state):
+        print("MOVING :: ", action)
+        state.board[action[0],action[1]] = 0
+        state.board[action[2],action[3]] = 0
+        state.board[action[4],action[5]] = 1
+        self.print_board(state)
         self.completed_moves.append(action)
 
 
-    def undo_move(self):
+    def undo_move(self, state):
         print("UNDO")
         action = self.completed_moves.pop()
-        self.board[action[0],action[1]] = 1
-        self.board[action[2],action[3]] = 1
-        self.board[action[4],action[5]] = 0
-        self.print_board()
+        # parent_board = np.full((self.board_size, self.board_size),3)
+        # parent_board[action[0],action[1]] = 1
+        # parent_board[action[2],action[3]] = 1
+        # parent_board[action[4],action[5]] = 0
+        # self.print_board(parent_board)
+        self.print_board(state)
+        self.completed_moves.pop()
+        
 
     # depth-first search
     def dfs(self):
-
+        # get initial state
+        parent_state = self.state_dictionary[0]
+        
         while self.won == False:
-            # Starting by getting the possuble moves
-            valid_moves = self.check_move()
+            # Starting by getting the possible moves
+            valid_moves = self.check_move(parent_state)
 
             # If there are no possible moves, we have to move up the tree, tracing back our steps. This is done by 
             # using the undo function
             if len(valid_moves) == 0:
-                self.undo_move()
-                valid_moves = self.check_move() # Gotta update the valid moves
+                parent_state = parent_state.parent
+                self.undo_move(parent_state)
+                valid_moves = self.check_move(parent_state) # Gotta update the valid moves
                 while self.frontier[-1] not in valid_moves: # To make the newest frontier corresponds to a current valid move
-                    self.undo_move()
-                    valid_moves = self.check_move()
+                    parent_state = parent_state.parent
+                    self.undo_move(parent_state)
+                    valid_moves = self.check_move(parent_state)
             else: # appending new nodes to our frontier
                 for ele in valid_moves:
                     self.frontier.append(ele)
 
             # Getting the next move from the frontier
             move = self.frontier.pop()
-            self.move(move) 
+
+            # create child and add to dictionary
+            new_state = State(self.board_size)
+            keys = list(parent_state.children.keys())
+            if keys:
+                key = keys[-1] + 1
+            else:
+                key = 0
+            parent_state.children = {key: new_state}
+            new_state.parent = parent_state
+            new_state.board = parent_state.board.copy()
+
+            self.move(move, new_state)
+            parent_state = new_state
             
-            if self.num_pegs() == 1:
+            if self.num_pegs(new_state) == 1:
                 print("Solution :: ",self.completed_moves)
                 print("Length of solution :: ", len(self.completed_moves))
                 self.won = True
 
-            self.dfs()
+            # self.dfs()
 
-    def A_Star(self):
+    def Dijkstra(self):
         self.frontier
         last_move = self.completed_moves[-1]
-
-        goal_cost = 0
         
         while self.won == False:
             # get the possible moves
